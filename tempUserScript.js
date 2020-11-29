fb5c5b55b82ae42f2cc1e4955301d6af19c6dcf3
MiniCoder11 <63173581+MiniCoder11@users.noreply.github.com>
2020-11-28 17:34:32 -0800

    Added TurboWarp compatibility

--- tempUserScript.js
+++ tempUserScript.js
@@ -5,12 +5,15 @@
 // @description  Detect any key!
 // @author       MiniCoder11
 // @match        https://*.scratch.mit.edu/*
-// @grant        none
+// @match        https://*.turbowarp.org/*
+// @grant        GM_addStyle
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    GM_addStyle(".sprite-selector_sprite-wrapper_1C5Mq { width: 380px !important; max-width: none !important; min-height: 0px !important; }");
 
     const findVM = () => new Promise(resolve => {
         const oldBind = Function.prototype.bind;
@@ -28,11 +31,9 @@
         window.vm = vm; // This allows experimentation in the console with the VM
 
         document.addEventListener('keydown', function (event) {
-            if (event.defaultPrevented) {
-                return;
-            }
             var key = event.key || event.keyCode;
             scratchVM.setVariableValue(scratchVM.runtime.getSpriteTargetByName("scratchModCompat").id,scratchVM.runtime.getSpriteTargetByName("scratchModCompat").lookupVariableByNameAndType("lastKeyPressed").id,key);
+            console.log(key);
         });
 
 
