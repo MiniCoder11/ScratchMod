e0a7454681af710c75610b9473ada156eae7394e
MiniCoder11 <63173581+MiniCoder11@users.noreply.github.com>
2020-12-02 20:33:17 -0800

    Update tempUserScript.js

--- tempUserScript.js
+++ tempUserScript.js
@@ -1,32 +1,61 @@
 // ==UserScript==
 // @name         ScratchMod
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.3.1
 // @description  Enhance the Scratch website with aesthetic and QoL changes.
 // @author       MiniCoder11
 // @match        https://*.scratch.mit.edu/*
 // @grant        GM_addStyle
-// @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
+//Apply styles and keep them up to date
+injectScript(`
+function addMenuItem(label, id, textColor) {
+    var newMenuItem = document.createElement("div");
+    newMenuItem.setAttribute("class", "menu-bar_menu-bar-item_oLDa-");
+    newMenuItem.innerHTML = '<span class="button_outlined-button_1bS__ menu-bar_menu-bar-button_3IDN0 community-button_community-button_2Lo_g" id="' + id + '"' + '" role="button">' + label + '</span>';
+    document.getElementsByClassName("menu-bar_main-menu_3wjWH")[0].insertBefore(newMenuItem, document.getElementsByClassName("menu-bar_menu-bar-item_oLDa-")[7]);
+    document.getElementById(id).style = "color: " + textColor + ";";
+}`); // <div class="button_content_3jdgj"><span id="' + id + '">' + label + '</span></div>
 
-    function formatLog(type, message) { console.log("[ScratchMod / " + type + "] " + message) } // Write a nicely formatted message to the console
+generateCSSVariables();
+applyStyles();
+keepStylingUpdated();
+initiateScratchMod();
 
-    if ($ != "") {formatLog("Info", "jQuery check successful.")} // Check if jQuery was imported successfully
-    formatLog("Info", "Loaded successfully!");
-    //document.addEventListener('keyup', function (event) { console.log(event.key); });
+var wireframeMode = false;
+var newElement;
+var currentButton;
+var wireframeButtonBack;
+var pageLoaded = false;
 
-    document.addEventListener('keyup', function (event) { if (event.ctrlKey && event.key === '`') {
-        applyStyles();
-        formatLog("Info", "Styles reloaded");
-    }});
+injectScript(`
+function addMenuItem(label, id, textColor) {
+    var newMenuItem = document.createElement("div");
+    newMenuItem.setAttribute("class", "menu-bar_menu-bar-item_oLDa-");
+    newMenuItem.innerHTML = '<span class="button_outlined-button_1bS__ menu-bar_menu-bar-button_3IDN0 community-button_community-button_2Lo_g" id="' + id + '"' + '" role="button">' + label + '</span>';
+    document.getElementsByClassName("menu-bar_main-menu_3wjWH")[0].insertBefore(newMenuItem, document.getElementsByClassName("menu-bar_menu-bar-item_oLDa-")[7]);
+    document.getElementById(id).style = "color: " + textColor + ";";
+}`); // <div class="button_content_3jdgj"><span id="' + id + '">' + label + '</span></div>
 
-    var oldURL = window.location.href;
-    setInterval(function() {if (oldURL != window.location.href) {applyStyles(); oldURL = window.location.href;}}, 500); //Reapply styling on URL change in case the page doesn't reload (from project page to editor)
+window.addEventListener('load', (function(){
+    createEditorButtons();
+    pageLoaded = true;
+}));
 
+function toggleWireframeMode() {
+    if(wireframeMode == true) {
+        newElement = document.createElement("style");
+        newElement.setAttribute("id", "wireframe-mode-style");
+        newElement.innerHTML = ".blocklyText, .blocklyFlyoutLabelText {fill: var(--text) !important;} .blocklyBlockBackground {fill: transparent;} .gui_body-wrapper_-N0sA * {filter: none;}";
+        document.head.appendChild(newElement);
+
+    } else {
+        document.getElementById("wireframe-mode-style").remove();
+    }
+}
+function generateCSSVariables() {
     // Define default styling and color palettes
     document.documentElement.style.setProperty('--background', '#1c1c28');
     document.documentElement.style.setProperty('--background-accent', '#28293d');
@@ -54,13 +83,72 @@
     linkElement.setAttribute('type', 'text/css');
     linkElement.setAttribute('href', 'https://fonts.googleapis.com/css?family=Rubik');
     document.head.appendChild(linkElement); // Check out "Inter" and "Questrial"
+}
+function createEditorButtons() {
+    formatLog("Info", "Editor buttons created");
+    addMenuItem("Open in Turbowarp", "open-in-turbowarp-button", "var(--red) !important");
+    currentButton = document.getElementById("open-in-turbowarp-button");
+    currentButton.onclick = function(){window.open(String("https://turbowarp.org/" + window.location.href.split("https://scratch.mit.edu/projects/")[1]).replace(" "), '_blank');}
 
-    applyStyles(); // Reapply/apply styles in one line
+    addMenuItem("Wireframe: Off", "wireframe-mode-toggle-button", "var(--green) !important");
+    currentButton = document.getElementById("wireframe-mode-toggle-button");
+    currentButton.onclick = (function(){wireframeMode = ! wireframeMode; if (wireframeMode) {currentButton.innerHTML = "Wireframe: On "} else {currentButton.innerHTML = "Wireframe: Off"}; toggleWireframeMode()});
+}
+function keepStylingUpdated() {
+    var oldURL = window.location.href;
+    setInterval(function() { if (oldURL != window.location.href) {applyStyles(); oldURL = window.location.href;}}, 500); //Reapply styling on URL change in case the page doesn't reload (from project page to editor)
+}
+function formatLog(type, message) {
+    // Write a nicely formatted message to the console
+    console.log("[ScratchMod / " + type + "] " + message)
+}
+function initiateScratchMod() {
+    // Initialize all special features and modules
+    GM_addStyle(".sprite-selector_sprite-wrapper_1C5Mq { width: 380px !important; max-width: none !important; min-height: 0px !important; }");
 
-    // Create page CSS and apply it
-    function applyStyles() {
+    const findVM = () => new Promise(resolve => {
+        const oldBind = Function.prototype.bind;
+        Function.prototype.bind = function(...args) {
+            if (args[0] && args[0].hasOwnProperty('editingTarget') && args[0].hasOwnProperty('runtime')) {
+                Function.prototype.bind = oldBind;
+                resolve(args[0]);
+            }
+            return oldBind.apply(this, args);
+        };
+    });
 
-        GM_addStyle(`
+    findVM().then(vm => {
+        var scratchVM = vm;
+        window.scratchVM = vm; // Exposes the VM to the console for experimentation
+        console.log(window.scratchVM)
+
+        //scratchVM.setVariableValue(scratchVM.runtime.getSpriteTargetByName("scratchModCompat").id,scratchVM.runtime.getSpriteTargetByName("scratchModCompat").lookupVariableByNameAndType("scratchModEnabled").id,"True");
+
+        document.addEventListener('keydown', function (event) {
+            var key = event.key || event.keyCode;
+            if (!(scratchVM.runtime.getAllVarNamesOfType().includes("lastKeyPressed"))) {scratchVM.setVariableValue(scratchVM.runtime.getSpriteTargetByName("scratchModCompat").id,scratchVM.runtime.getSpriteTargetByName("scratchModCompat").lookupVariableByNameAndType("lastKeyPressed").id,key);}
+        });
+
+        formatLog("Info", "Styling complete.");
+
+    });
+
+    //addMenuItem("Turn on Wireframe Blocks", "scratchmod-wireframe-mode-button", "lime");
+    //console.log("Menu Item")
+    //var button = document.getElementById("scratchmod-wireframe-mode-button").onclick(function(){
+    //    wireframeMode = ! wireframeMode;
+    //    if (wireframeMode) {button.innerHTML = "Turn off Wireframe Blocks"} else {button.innerHTML = "Turn on Wireframe Blocks"}
+    //});
+}
+function injectScript(javascript) {
+    // Inject JavaScript to the head of the page
+    var newScriptNode = document.createElement("script");
+    newScriptNode.innerHTML = javascript;
+    document.head.appendChild(newScriptNode);
+}
+function applyStyles() {
+    // Create page CSS and editor buttons and apply them
+    GM_addStyle(`
     /* General Site Styling */
 
     * {font-family: "Rubik" !important;}
@@ -94,8 +182,8 @@
     .comment-text, .comment .comment-body .comment-bubble, .preview .guiPlayer .project-info-alert, .comment .comment-body .comment-bubble::before {background-color: var(--background) !important;}
     .inplace-textarea {background-color: var(--background) !important; color: var(--text) !important;}
     `);
-        if (window.location.href.includes("editor")) {
-            GM_addStyle(`
+    if (window.location.href.includes("editor")) {
+        GM_addStyle(`
         /* Editor Styles Start Here */
     .scratchCategoryMenuItemLabel, .blocklyFlyoutLabelText {color: var(--text)}
     .loader_background_2DPrW, .blocklyToolboxDiv, .scratchCategoryMenuItem, .sprite-selector_sprite-selector_2KgCX, .selector_wrapper_8_BHs {background: var(--background) !important;} /* Loading screen backdrop */
@@ -121,15 +209,7 @@
     .prompt_body_18Z-I {background: var(--editor-accent) !important; opacity: 1 !important;} /* Variable/list creation popup */
     [dir="ltr"] .modal_header-item-title_tLOU5, .custom-procedures_body_SQBv6 {background: var(--background);}
     .modal_modal-overlay_1Lcbx {background-color: #1c1c28d6 !important; backdrop-filter: blur(4px) !important;}
-    `);}
-
-        if (window.location.href.includes("mystuff")) {
-            GM_addStyle(`
-        /* Mystuff Page */
-    * {border: none !important; text-shadow: none;}
-    #footer {box-shadow: none;}
-    a[href] {color: var(--blue) !important;}
-    `);}
+    `);
 
         if (window.location.href.includes("messages")) {
             GM_addStyle(`
@@ -137,32 +217,10 @@
         `)
         }
 
-        // ========================================= ScratchMod Experimental Features =========================================
-        GM_addStyle(".sprite-selector_sprite-wrapper_1C5Mq { width: 380px !important; max-width: none !important; min-height: 0px !important; }");
-
-        const findVM = () => new Promise(resolve => {
-            const oldBind = Function.prototype.bind;
-            Function.prototype.bind = function(...args) {
-                if (args[0] && args[0].hasOwnProperty('editingTarget') && args[0].hasOwnProperty('runtime')) {
-                    Function.prototype.bind = oldBind;
-                    resolve(args[0]);
-                }
-                return oldBind.apply(this, args);
-            };
-        });
-
-        findVM().then(vm => {
-            var scratchVM = vm;
-            window.vm = vm; // Exposes the VM to the console for experimentation
-
-            //scratchVM.setVariableValue(scratchVM.runtime.getSpriteTargetByName("scratchModCompat").id,scratchVM.runtime.getSpriteTargetByName("scratchModCompat").lookupVariableByNameAndType("scratchModEnabled").id,"True");
-
-            document.addEventListener('keydown', function (event) {
-                var key = event.key || event.keyCode;
-                scratchVM.setVariableValue(scratchVM.runtime.getSpriteTargetByName("scratchModCompat").id,scratchVM.runtime.getSpriteTargetByName("scratchModCompat").lookupVariableByNameAndType("lastKeyPressed").id,key);
-            });
-
-            formatLog("Info", "Styling complete.");
-
-        }
-                     )}})();
+        if (window.location.href.includes("mystuff")) {
+            GM_addStyle(`
+        /* Mystuff Page */
+    * {border: none !important; text-shadow: none;}
+    #footer {box-shadow: none;}
+    a[href] {color: var(--blue) !important;}
+    `);}}}
